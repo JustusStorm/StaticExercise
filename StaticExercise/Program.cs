b95bf9db6e727ec84d40f610d8949bfa158390bd
Justus Storm <storm.j.dev@gmail.com>
2020-09-03 15:01:02 -0500

    Completed Exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = 100;
+
+            var fTemp = TempConverter.CelsiusToFahrenheit(temp);
+            Console.WriteLine(fTemp);
+
+            var cTemp = TempConverter.FahrenheitToCelsius(fTemp);
+            Console.WriteLine(cTemp);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    class TempConverter
+    {
+
+        public static int CelsiusToFahrenheit(int temp)
+        {
+             return (((temp / 5) * 9) + 32);
+        }
+
+        public static int FahrenheitToCelsius(int temp)
+        {
+            return (((temp - 32) * 5) / 9);
+        }
+    }
+}
